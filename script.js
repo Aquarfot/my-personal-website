fdc6c2c4f21bbee2da0c5701c1440cd24d79a56e
Andrew Quarfot <andrew.quarfot@gmail.com>
2022-05-09 12:49:28 -0600

    added JS elements

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // const h4 = document.createElement("h4");
 // h4.textContent = "Connecticut Born, Colorado Sworn";
 // document.querySelector("body").appendChild(h4);
+
+let highlightItem = document.getElementById("copyrightInfo");
+copyrightInfo.addEventListener("mouseover", function(event) {
+    event.target.style.color = "green";
+    setTimeout(function() {
+        event.target.style.color = " ";
+    }, 500);
+}, false);
+
